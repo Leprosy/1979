035cd73dcd9f3c9e6c1859d3a2cefe14a2d264c2
Miguel Rojas <mrojas2@evernote.com>
2022-06-03 16:56:13 -0400

    Added screen limits

--- src/helpers/playerController.ts
+++ src/helpers/playerController.ts
@@ -1,14 +1,22 @@
+import { isInsideScreen } from "./screen";
+
 export const playerController = (keys: Phaser.Types.Input.Keyboard.CursorKeys, player: Phaser.GameObjects.Rectangle, speed: number) => {
+  let nx = player.x;
+  let ny = player.y;
   if (keys.up.isDown) {
-    player.y -= speed;
+    ny -= speed;
   }
   if (keys.down.isDown) {
-    player.y += speed;
+    ny += speed;
   }
   if (keys.left.isDown) {
-    player.x -= speed;
+    nx -= speed;
   }
   if (keys.right.isDown) {
-    player.x += speed;
+    nx += speed;
+  }
+
+  if (isInsideScreen(nx, ny)) {
+    player.setPosition(nx, ny);
   }
 };
