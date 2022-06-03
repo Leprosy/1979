63662c6e06f6604e30491188ccee3f817b992cf3
Miguel Rojas <mrojas2@evernote.com>
2022-06-03 16:56:41 -0400

    Bullets as circles(debug, these will be sprites)

--- src/entities/Bullet.ts
+++ src/entities/Bullet.ts
@@ -1,12 +1,13 @@
 import Phaser from "phaser";
 import { isInsideScreen } from "../helpers/screen";
 
-export class Bullet extends Phaser.GameObjects.Rectangle {
+export class Bullet extends Phaser.GameObjects.Arc {
   speedX: number;
   speedY: number;
 
   constructor(scene: Phaser.Scene, origin: Phaser.Types.Math.Vector2Like, target: Phaser.Types.Math.Vector2Like, speed: number) {
-    super(scene, origin.x, origin.y, 5, 5, 0xff0000);
+    super(scene, origin.x, origin.y, 5);
+    this.setFillStyle(0xff0000);
     const angle = Phaser.Math.Angle.BetweenPoints(origin, target);
     this.speedX = speed * Math.cos(angle);
     this.speedY = speed * Math.sin(angle);
@@ -17,7 +18,6 @@ export class Bullet extends Phaser.GameObjects.Rectangle {
     this.x += this.speedX;
 
     if (!isInsideScreen(this.x, this.y)) {
-      console.log("splat");
       this.destroy();
     }
   }
