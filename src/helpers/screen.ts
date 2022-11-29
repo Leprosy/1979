8bda6f09f1286d19735f0980b8843fc0ee10dbb0
Miguel Rojas <mrojas2@evernote.com>
2022-11-29 17:35:10 -0300

    A helper to check collitions

--- src/helpers/screen.ts
+++ src/helpers/screen.ts
@@ -3,3 +3,9 @@ import { gameConfig } from "../config";
 export const isInsideScreen = (x: number, y: number) => {
   return y >= 0 && x >= 0 && y <= gameConfig.height && x <= gameConfig.width;
 };
+
+export const areColliding = (a: any, b: any) => {
+  const boundsA = a.getBounds();
+  const boundsB = b.getBounds();
+  return Phaser.Geom.Intersects.RectangleToRectangle(boundsA, boundsB);
+};
