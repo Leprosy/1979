1d76afbd0a3dabff09f271e804d6c91b069167a6
Miguel Rojas <mrojas2@evernote.com>
2022-12-20 16:49:29 -0300

    Base Actor class

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -1,48 +1,15 @@
 import Phaser from 'phaser';
+import { Actor } from './Actor';
 
-export class Player extends Phaser.GameObjects.Sprite {
-  hp: number;
-
+export class Player extends Actor {
   constructor(scene: Phaser.Scene) {
-    super(scene, 0, 0, 'plane', 1);
+    super(scene, 'plane');
 
     // Setup
     this.hp = 3; // TODO: config?
     this.x = 100;
     this.y = 500;
 
-    // Animations
-    this.anims.create({
-      key: 'idle',
-      frames: this.anims.generateFrameNumbers('plane', { frames: [0, 1] }),
-      frameRate: 8,
-      repeat: -1,
-    });
-    this.anims.create({
-      key: 'death',
-      frames: this.anims.generateFrameNumbers('plane', { frames: [6, 7, 8, 9, 10, 11] }),
-      frameRate: 2,
-      repeat: 0,
-    });
-    this.setVisible(true);
-    this.play('idle');
     scene.add.existing(this); // This make player visible
-
-    // Events
-    this.on('animationcomplete', () => {
-      if (this.anims.getName() == 'death') {
-        this.destroy();
-      }
-    });
-  }
-
-  update() {
-    if (this.hp <= 0 && this.anims && this.anims.getName() != 'death') {
-      this.play('death');
-    }
-  }
-
-  isDead() {
-    return this.hp <= 0;
   }
 }
